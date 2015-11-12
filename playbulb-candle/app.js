a79170fff504d0718c112e531d664101f7758909
Francois Beaufort <beaufort.francois@gmail.com>
2015-11-12 16:59:12 +0100

    Added PLAYBULB Candle Demo

---
+++ playbulb-candle/app.js
@@ -0,0 +1,111 @@
+var r = g = b = 255; // White by default.
+
+document.querySelector('#connect').addEventListener('click', function() {
+  document.querySelector('#state').classList.add('connecting');
+  playbulbCandle.connect()
+  .then(() => {
+    document.querySelector('#state').classList.remove('connecting');
+    document.querySelector('#state').classList.add('connected');
+    return playbulbCandle.getDeviceName().then(handleDeviceName)
+    .then(() => playbulbCandle.getBatteryLevel().then(handleBatteryLevel));
+  })
+  .catch(error => {
+    // TODO: Replace with toast when snackbar lands.
+    console.error('Argh!', error);
+  });
+});
+
+function handleDeviceName(deviceName) {
+  document.querySelector('#deviceName').value = deviceName;
+}
+
+function handleBatteryLevel(batteryLevel) {
+  document.querySelector('#batteryLevel').textContent = batteryLevel + '%';
+}
+
+/* Device name */
+
+document.querySelector('#deviceName').addEventListener('input', function() {
+  playbulbCandle.setDeviceName(this.value)
+  .catch(error => {
+    console.error('Argh!', error);
+  });
+});
+
+/* Color picker */
+
+var img = new Image();
+img.src = 'color-wheel.png'
+img.onload = function() {
+  var canvas = document.querySelector('canvas');
+  var context = canvas.getContext('2d');
+
+  canvas.width = 300 * devicePixelRatio;
+  canvas.height = 300 * devicePixelRatio;
+  canvas.style.width = "300px";
+  canvas.style.height = "300px";
+  canvas.addEventListener('click', function(evt) {
+    // Refresh canvas in case user zooms and devicePixelRatio changes.
+    canvas.width = 300 * devicePixelRatio;
+    canvas.height = 300 * devicePixelRatio;
+    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+    var rect = canvas.getBoundingClientRect();
+    var x = Math.round((evt.clientX - rect.left) * devicePixelRatio);
+    var y = Math.round((evt.clientY - rect.top) * devicePixelRatio);
+    var data = context.getImageData(0, 0, canvas.width, canvas.height).data;
+
+    r = data[((canvas.width * y) + x) * 4];
+    g = data[((canvas.width * y) + x) * 4 + 1];
+    b = data[((canvas.width * y) + x) * 4 + 2];
+
+    changeColor();
+  });
+
+  context.drawImage(img, 0, 0, canvas.width, canvas.height);
+}
+
+/* Color effects */
+
+document.querySelector('#noEffect').addEventListener('click', changeColor);
+document.querySelector('#candleEffect').addEventListener('click', changeColor);
+document.querySelector('#flashing').addEventListener('click', changeColor);
+document.querySelector('#pulse').addEventListener('click', changeColor);
+document.querySelector('#rainbow').addEventListener('click', changeColor);
+document.querySelector('#rainbowFade').addEventListener('click', changeColor);
+
+function changeColor() {
+  var effect = document.querySelector('[name="effectSwitch"]:checked').id;
+  switch(effect) {
+    case 'noEffect':
+      playbulbCandle.setColor(r, g, b).then(onColorChanged);
+      break;
+    case 'candleEffect':
+      playbulbCandle.setCandleEffectColor(r, g, b).then(onColorChanged);
+      break;
+    case 'flashing':
+      playbulbCandle.setFlashingColor(r, g, b).then(onColorChanged);
+      break;
+    case 'pulse':
+      playbulbCandle.setPulseColor(r, g, b).then(onColorChanged);
+      break;
+    case 'rainbow':
+      playbulbCandle.setRainbow().then(onColorChanged);
+      break;
+    case 'rainbowFade':
+      playbulbCandle.setRainbowFade().then(onColorChanged);
+      break;
+  }
+}
+
+function onColorChanged(rgb) {
+  if (rgb) {
+    console.log('Color changed to ' + rgb);
+    r = rgb[0];
+    g = rgb[1];
+    b = rgb[2];
+  } else {
+    console.log('Color changed');
+  }
+}
+
---
+++ playbulb-candle/playbulbCandle.js
@@ -0,0 +1,175 @@
+(() => {
+  'use strict';
+
+  let encoder = new TextEncoder('utf-8');
+  let decoder = new TextDecoder('utf-8');
+
+  /* Custom Bluetooth Service UUIDs */
+
+  const CANDLE_SERVICE_UUID = 0xFF02;
+
+  /* Custom Bluetooth Characteristic UUIDs */
+
+  const CANDLE_DEVICE_NAME_UUID = 0xFFFF;
+  const CANDLE_COLOR_UUID = 0xFFFC;
+  const CANDLE_EFFECT_UUID = 0xFFFB;
+
+  class PlaybulbCandle {
+    constructor() {
+      this.device = null;
+      this.server = null;
+      this._characteristics = new Map();
+      this._debug = false;
+    }
+    connect() {
+      return navigator.bluetooth.requestDevice({filters:[{services:[ CANDLE_SERVICE_UUID ]}]})
+      .then(device => {
+        this.device = device;
+        return device.connectGATT();
+      })
+      .then(server => {
+        this.server = server;
+        return Promise.all([
+          server.getPrimaryService(CANDLE_SERVICE_UUID).then(service => {
+            return Promise.all([
+              this._cacheCharacteristic(service, CANDLE_DEVICE_NAME_UUID),
+              this._cacheCharacteristic(service, CANDLE_COLOR_UUID),
+              this._cacheCharacteristic(service, CANDLE_EFFECT_UUID),
+            ])
+          }),
+          server.getPrimaryService('battery_service').then(service => {
+            return this._cacheCharacteristic(service, 'battery_level')
+          }),
+          // TODO: Uncomment when device_information service is actually
+          // available in Chrome OS. http://crbug.com/532930
+          /*
+          server.getPrimaryService('device_information').then(service => {
+            return Promise.all([
+              this._cacheCharacteristic(service, 'serial_number_string'),
+              this._cacheCharacteristic(service, 'hardware_revision_string'),
+              this._cacheCharacteristic(service, 'firmware_revision_string'),
+              this._cacheCharacteristic(service, 'software_revision_string'),
+              this._cacheCharacteristic(service, 'manufacturer_name_string'),
+              this._cacheCharacteristic(service, 'pnp_id'),
+            ])
+          }),
+          */
+        ]);
+      })
+    }
+
+    /* Candle Service */
+
+    getDeviceName() {
+      return this._readCharacteristicValue(CANDLE_DEVICE_NAME_UUID)
+      .then(this._decodeString);
+    }
+    setDeviceName(name) {
+      let data = this._encodeString(name);
+      return this._writeCharacteristicValue(CANDLE_DEVICE_NAME_UUID, data)
+    }
+    setColor(r, g, b) {
+      let data = [0x00, r, g, b];
+      return this._writeCharacteristicValue(CANDLE_COLOR_UUID, new Uint8Array(data))
+      .then(() => [r,g,b]); // Returns color when fulfilled.
+    }
+    setCandleEffectColor(r, g, b) {
+      let data = [0x00, r, g, b, 0x04, 0x00, 0x01, 0x00];
+      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data))
+      .then(() => [r,g,b]); // Returns color when fulfilled.
+    }
+    setFlashingColor(r, g, b) {
+      let data = [0x00, r, g, b, 0x00, 0x00, 0x1F, 0x00];
+      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data))
+      .then(() => [r,g,b]); // Returns color when fulfilled.
+    }
+    setPulseColor(r, g, b) {
+      // We have to correct user color to make it look nice for real...
+      let newRed = Math.min(Math.round(r / 64) * 64, 255);
+      let newGreen = Math.min(Math.round(g / 64) * 64, 255);
+      let newBlue = Math.min(Math.round(b / 64) * 64, 255);
+      let data = [0x00, newRed, newGreen, newBlue, 0x01, 0x00, 0x09, 0x00];
+      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data))
+      .then(() => [r,g,b]); // Returns color when fulfilled.
+    }
+    setRainbow() {
+      let data = [0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0x00];
+      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data));
+    }
+    setRainbowFade() {
+      let data = [0x01, 0x00, 0x00, 0x00, 0x03, 0x00, 0x26, 0x00];
+      return this._writeCharacteristicValue(CANDLE_EFFECT_UUID, new Uint8Array(data));
+    }
+
+    /* Battery Service */
+
+    getBatteryLevel() {
+      return this._readCharacteristicValue('battery_level')
+      .then(data => data.getUint8(0));
+    }
+
+    /* Device Info Service */
+
+    getSerialNumber() {
+      return this._readCharacteristicValue('serial_number_string')
+      .then(this._decodeString);
+    }
+    getHardwareRevision() {
+      return this._readCharacteristicValue('hardware_revision_string')
+      .then(this._decodeString);
+    }
+    getFirmwareRevision() {
+      return this._readCharacteristicValue('firmware_revision_string')
+      .then(this._decodeString);
+    }
+    getSoftwareRevision() {
+      return this._readCharacteristicValue('software_revision_string')
+      .then(this._decodeString);
+    }
+    getManufacturerName() {
+      return this._readCharacteristicValue('manufacturer_name_string')
+      .then(this._decodeString);
+    }
+    getPnpID() {
+      return this._readCharacteristicValue('pnp_id')
+      .then(this._decodeString);
+    }
+
+    /* Utils */
+
+    _cacheCharacteristic(service, characteristicUuid) {
+      return service.getCharacteristic(characteristicUuid)
+      .then(characteristic => {
+        this._characteristics.set(characteristicUuid, characteristic);
+      });
+    }
+    _readCharacteristicValue(characteristicUuid) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      return characteristic.readValue()
+      .then(buffer => {
+        let data = new DataView(buffer);
+        if (this._debug) {
+          for (var i = 0, a = []; i < data.byteLength; i++) { a.push(data.getUint8(i)); }
+          console.debug('READ', characteristic.uuid, a);
+        }
+        return data;
+      });
+    }
+    _writeCharacteristicValue(characteristicUuid, value) {
+      let characteristic = this._characteristics.get(characteristicUuid);
+      if (this._debug) {
+        console.debug('WRITE', characteristic.uuid, value);
+      }
+      return characteristic.writeValue(value);
+    }
+    _decodeString(data) {
+      return decoder.decode(data);
+    }
+    _encodeString(data) {
+      return encoder.encode(data);
+    }
+  }
+
+  window.playbulbCandle = new PlaybulbCandle();
+
+})();
