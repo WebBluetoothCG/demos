06748ed84a186c486be0df3dacdff7525fa54369
François Beaufort <beaufort.francois@gmail.com>
2016-08-17 15:03:34 +0200

    Improve SW readibility (#56)

--- playbulb-candle/sw.js
+++ playbulb-candle/sw.js
@@ -1,27 +1,40 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('playbulb-candle').then(cache => {
-      return cache.match(event.request).then(response => {
+const addToCache = (request, networkResponse) => {
+  return caches.open('playbulb-candle')
+    .then((cache) => cache.put(request, networkResponse.clone()));
+};
 
-        // If a request doesn't match anything in the cache, get it from the
-        // network, send it to the page & add it to the cache at the same time.
-        return response || fetch(event.request).then(response => {
-          cache.put(event.request, response.clone());
-          return response;
-        });
+const getCacheResponse = (request) => {
+  return caches.open('playbulb-candle').then((cache) => {
+    return cache.match(request);
+  });
+};
+
+const getNetworkOrCacheResponse = (request) => {
+  return new Promise((resolve) => {
+    fetch(request).then((networkResponse) => {
+      addToCache(request, networkResponse);
+      resolve(networkResponse);
+    }).catch(() => {
+      getCacheResponse(request).then((cacheResponse) => {
+        resolve(cacheResponse || Response.error());
       });
-    })
-  );
+    });
+  });
+};
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(getNetworkOrCacheResponse(event.request));
 });
 
-/* Clean up old caches */
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(cacheName => (cacheName !== 'playbulb-candle'))
-                  .map(cacheName => caches.delete(cacheName))
-      );
-    })
-  );
+const cleanOldCache = () => {
+  return caches.keys().then(cacheNames => {
+    return Promise.all(
+      cacheNames.filter(cacheName => (cacheName !== 'playbulb-candle'))
+                .map(cacheName => caches.delete(cacheName))
+    );
+  })
+}
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(cleanOldCache());
 });
